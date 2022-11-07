cf05b809af8f2ecd0bd6f42e1eafde84f4f81e2d
Caerfyre <ninathereseybas@gmail.com>
2022-11-07 14:38:48 +0800

    set field container size & fix obscurePass value

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -13,31 +13,36 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passController = TextEditingController();
-  bool obscurePass = false;
+  bool obscurePass = true;
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: Container(
         child: Center(
           child: SingleChildScrollView(
             child: Center(
-              child: Column(
-                children: [
-                  CustTextField(
-                      labelText: "Email Address",
-                      hintText: "Enter Email Address",
-                      controller: emailController,
-                      textInputType: TextInputType.emailAddress),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  PasswordField(
-                      obscureText: obscurePass,
-                      onTap: handleObscurePass,
-                      labelText: "Password",
-                      hintText: "Enter Password",
-                      controller: passController)
-                ],
+              child: Container(
+                width: screenWidth * .9,
+                child: Column(
+                  children: [
+                    CustTextField(
+                        labelText: "Email Address",
+                        hintText: "Enter Email Address",
+                        controller: emailController,
+                        textInputType: TextInputType.emailAddress),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    PasswordField(
+                        obscureText: obscurePass,
+                        onTap: handleObscurePass,
+                        labelText: "Password",
+                        hintText: "Enter Password",
+                        controller: passController)
+                  ],
+                ),
               ),
             ),
           ),
