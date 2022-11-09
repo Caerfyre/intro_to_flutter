b524c383cdc01cb4596106270372ac5b6d23753f
Caerfyre <ninathereseybas@gmail.com>
2022-11-10 02:23:12 +0800

    add login button

--- lib/screens/SignUpScreen.dart
+++ lib/screens/SignUpScreen.dart
@@ -114,6 +114,13 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     const SizedBox(
                       height: 20,
                     ),
+                    CustButton(
+                        labelText: "Login",
+                        iconData: Icons.login,
+                        onPress: () {}),
+                    const SizedBox(
+                      height: 20,
+                    ),
                   ],
                 ),
               ),
