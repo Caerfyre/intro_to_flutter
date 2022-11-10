e61ba6d060fd219550f45ff1a009b923444c856d
Caerfyre <ninathereseybas@gmail.com>
2022-11-10 11:52:07 +0800

    change class name and add route

--- lib/screens/SettingsScreen.dart
+++ lib/screens/SettingsScreen.dart
@@ -1,14 +1,16 @@
 import 'package:bs_flutter_buttons/bs_flutter_buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:intro_to_flutter/screens/LoginScreen.dart';
 
-class Settings extends StatefulWidget {
-  const Settings({super.key});
+class SettingsScreen extends StatefulWidget {
+  static String routeName = "/settings";
+  const SettingsScreen({super.key});
 
   @override
-  State<Settings> createState() => _SettingsState();
+  State<SettingsScreen> createState() => _SettingsState();
 }
 
-class _SettingsState extends State<Settings> {
+class _SettingsState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,18 +20,11 @@ class _SettingsState extends State<Settings> {
           child: Wrap(
             children: [
               BsButton(
-                margin: EdgeInsets.only(right: 10.0, bottom: 0.0),
-                onPressed: () {},
-                style: BsButtonStyle.primary,
-                size: BsButtonSize(
-                  padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
-                ),
-                prefixIcon: Icons.arrow_back,
-                label: Text('Back'),
-              ),
-              BsButton(
                 margin: EdgeInsets.only(right: 10.0, bottom: 10.0),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                      LoginScreen.routeName, (Route<dynamic> route) => false);
+                },
                 style: BsButtonStyle.primary,
                 size: BsButtonSize(
                   padding: const EdgeInsets.fromLTRB(8, 5, 8, 5),
