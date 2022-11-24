4bf9b775a9d173c81b204435c6e92ecb74148925
Caerfyre <ninathereseybas@gmail.com>
2022-11-25 01:34:19 +0800

    add firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intro_to_flutter/routes.dart';
 import 'package:intro_to_flutter/screens/LoginScreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: LoginScreen.routeName,
