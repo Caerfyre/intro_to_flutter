f89fec8dbb2c65ecc48fcf3575c5db77f4e8097a
Caerfyre <ninathereseybas@gmail.com>
2022-12-17 16:55:04 +0800

    add modal progress hud

--- lib/screens/SettingsScreen.dart
+++ lib/screens/SettingsScreen.dart
@@ -3,6 +3,7 @@ import 'package:intro_to_flutter/screens/LoginScreen.dart';
 import 'package:intro_to_flutter/services/AuthService.dart';
 import 'package:intro_to_flutter/services/StorageService.dart';
 import 'package:intro_to_flutter/widgets/CustButton.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class SettingsScreen extends StatefulWidget {
   static String routeName = "/settings";
@@ -13,8 +14,8 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsState extends State<SettingsScreen> {
-  AuthService _authService = AuthService();
-  StorageService _storageService = StorageService();
+  final AuthService _authService = AuthService();
+  final StorageService _storageService = StorageService();
   bool showSpinner = false;
 
   @override
@@ -23,35 +24,39 @@ class _SettingsState extends State<SettingsScreen> {
 
     return Scaffold(
       appBar: AppBar(title: const Text("Settings"), elevation: 10.0),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Center(
-            child: SizedBox(
-              width: screenWidth * .9,
-              child: Column(
-                children: [
-                  CustButton(
-                    labelText: "Logout",
-                    iconData: Icons.logout,
-                    onPress: () async {
-                      try {
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Center(
+              child: SizedBox(
+                width: screenWidth * .9,
+                child: Column(
+                  children: [
+                    CustButton(
+                      labelText: "Logout",
+                      iconData: Icons.logout,
+                      onPress: () async {
+                        try {
+                          setState(() {
+                            showSpinner = true;
+                          });
+                          await _authService.logout();
+                          await _storageService.deleteAllData();
+                          // ignore: use_build_context_synchronously
+                          Navigator.of(context).pushNamedAndRemoveUntil(
+                              LoginScreen.routeName,
+                              (Route<dynamic> route) => false);
+                        } catch (e) {
+                          print(e);
+                        }
                         setState(() {
-                          showSpinner = true;
+                          showSpinner = false;
                         });
-                        await _authService.logout();
-                        await _storageService.deleteAllData();
-                        Navigator.of(context).pushNamedAndRemoveUntil(
-                            LoginScreen.routeName,
-                            (Route<dynamic> route) => false);
-                      } catch (e) {
-                        print(e);
-                      }
-                      setState(() {
-                        showSpinner = false;
-                      });
-                    },
-                  ),
-                ],
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
